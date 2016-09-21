78c586c1ab25188d249d6702f0812ba874126ebf
fedolin <fedolin90@gmail.com>
2016-09-21 20:20:27 +0530

    changes to add functions for arithematic operations

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+func add(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func div(a, b int) int {
+	return a / b
+}
+
+func mod(a, b int) int {
+	return a % b
+}
+
 func main() {
 	fmt.Println("Calculator Web Application")
 }
